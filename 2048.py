ebcbd3f892681e285f78af9f9763d164ed9bff2e
chunzhi128218 <34428054+chunzhi128218@users.noreply.github.com>
2020-04-07 14:43:47 +0800

    Update 2048.py

--- 2048.py
+++ 2048.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 
+# curses 用来在终端上显示图形界面
 import curses
-from random import randrange, choice  # generate and place new tile
+# random 模块用来生成随机数
+from random import randrange, choice
+# collections 提供了一个字典的子类 defaultdict。可以指定 key 值不存在时，value 的默认值。
 from collections import defaultdict
 
+# ord() 函数以一个字符作为参数，返回参数对应的 ASCII 数值，便于和后面捕捉的键位关联
 letter_codes = [ord(ch) for ch in 'WASDRQwasdrq']
 # print(letter_codes)
 actions = ['Up', 'Left', 'Down', 'Right', 'Restart', 'Exit']
@@ -12,14 +16,13 @@ actions_dict = dict(zip(letter_codes, actions * 2))
 
 # print(actions_dict)
 
-def get_user_action(keyboard):
-    char = 'N'
-    while char not in actions_dict:
-        # 返回按下键的 ascii 码值
+def get_user_action(keyboard):    
+    char = "N"
+    while char not in actions_dict:  
+        # 返回按下键位的 ASCII　码值
         char = keyboard.getch()
-
+    # 返回输入键位对应的行为
     return actions_dict[char]
-
 
 def transpose(field):
     return [list(row) for row in zip(*field)]
@@ -31,17 +34,19 @@ def invert(field):
 
 class GameField(object):
     def __init__(self, height=4, width=4, win=2048):
-        self.height = height
-        self.width = width
-        self.win_value = win
-        self.score = 0
-        self.highscore = 0
-        self.reset()
+        self.height = height       # 高
+        self.width = width         # 宽
+        self.win_value = 2048      # 过关分数
+        self.score = 0             # 当前分数
+        self.highscore = 0         # 最高分
+        self.reset()               # 棋盘重置
 
     def reset(self):
+        # 更新分数
         if self.score > self.highscore:
             self.highscore = self.score
         self.score = 0
+        # 初始化游戏开始界面
         self.field = [[0 for i in range(self.width)] for j in range(self.height)]
         self.spawn()
         self.spawn()
@@ -49,35 +54,46 @@ class GameField(object):
     def move(self, direction):
         def move_row_left(row):
             def tighten(row):  # squeese non-zero elements together
+                '''把零散的非零单元挤到一块'''
+                # 先将非零的元素全拿出来加入到新列表
                 new_row = [i for i in row if i != 0]
+                # 按照原列表的大小，给新列表后面补零
                 new_row += [0 for i in range(len(row) - len(new_row))]
                 return new_row
 
             def merge(row):
+                 '''对邻近元素进行合并'''
                 pair = False
                 new_row = []
                 for i in range(len(row)):
                     if pair:
+                        # 合并后，加入乘 2 后的元素在 0 元素后面
                         new_row.append(2 * row[i])
+                        # 更新分数
                         self.score += 2 * row[i]
                         pair = False
                     else:
+                        # 判断邻近元素能否合并
                         if i + 1 < len(row) and row[i] == row[i + 1]:
                             pair = True
+                            # 可以合并时，新列表加入元素 0
                             new_row.append(0)
                         else:
+                            # 可以合并时，新列表加入元素 0
                             new_row.append(row[i])
+                # 断言合并后不会改变行列大小，否则报错
                 assert len(new_row) == len(row)
                 return new_row
-
+            # 先挤到一块再合并再挤到一块
             return tighten(merge(tighten(row)))
-
+        
+        # 创建 moves 字典，把不同的棋盘操作作为不同的 key，对应不同的方法函数
         moves = {}
         moves['Left'] = lambda field: [move_row_left(row) for row in field]
         moves['Right'] = lambda field: invert(moves['Left'](invert(field)))
         moves['Up'] = lambda field: transpose(moves['Left'](transpose(field)))
         moves['Down'] = lambda field: transpose(moves['Right'](transpose(field)))
-
+        # 判断棋盘操作是否存在且可行
         if direction in moves:
             if self.move_is_possible(direction):
                 self.field = moves[direction](self.field)
@@ -87,9 +103,11 @@ class GameField(object):
                 return False
 
     def is_win(self):
+        # 任意一个位置的数大于设定的 win 值时，游戏胜利
         return any(any(i >= self.win_value for i in row) for row in self.field)
 
     def is_gameover(self):
+        # 无法移动和合并时，游戏失败
         return not any(self.move_is_possible(move) for move in actions)
 
     def draw(self, screen):
@@ -98,9 +116,12 @@ class GameField(object):
         gameover_string = '           GAME OVER'
         win_string = '          YOU WIN!'
 
+        # 绘制函数
         def cast(string):
+            # addstr() 方法将传入的内容展示到终端
             screen.addstr(string + '\n')
 
+        # 绘制水平分割线的函数
         def draw_hor_separator():
             line = '+' + ('+------' * self.width + '+')[1:]
             separator = defaultdict(lambda: line)
@@ -109,17 +130,23 @@ class GameField(object):
             cast(separator[draw_hor_separator.counter])
             draw_hor_separator.counter += 1
 
+        # 绘制竖直分割线的函数
         def draw_row(row):
             cast(''.join('|{: ^5} '.format(num) if num > 0 else '|      ' for num in row) + '|')
 
+        # 清空屏幕
         screen.clear()
+        # 绘制分数和最高分
         cast('SCORE: ' + str(self.score))
         if 0 != self.highscore:
             cast('HIGHSCORE: ' + str(self.highscore))
+        # 绘制行列边框分割线
         for row in self.field:
             draw_hor_separator()
             draw_row(row)
         draw_hor_separator()
+        
+         # 绘制提示文字
         if self.is_win():
             cast(win_string)
         else:
@@ -135,19 +162,29 @@ class GameField(object):
         self.field[i][j] = new_element
 
     def move_is_possible(self, direction):
+        '''传入要移动的方向
+        判断能否向这个方向移动
+        '''
         def row_is_left_movable(row):
+            '''判断一行里面能否有元素进行左移动或合并
+            '''
             def change(i):  # true if there'll be change in i-th tile
+                # 当左边有空位（0），右边有数字时，可以向左移动
                 if row[i] == 0 and row[i + 1] != 0:  # Move
                     return True
+                # 当左边有一个数和右边的数相等时，可以向左合并
                 if row[i] != 0 and row[i + 1] == row[i]:  # Merge
                     return True
                 return False
 
             return any(change(i) for i in range(len(row) - 1))
-
+        
+        # 检查能否移动（合并也可以看作是在移动）
         check = {}
+        # 判断矩阵每一行有没有可以左移动的元素
         check['Left'] = lambda field: \
             any(row_is_left_movable(row) for row in field)
+        # 判断矩阵每一行有没有可以右移动的元素。这里只用进行判断，所以矩阵变换之后，不用再变换复原
 
         check['Right'] = lambda field: \
             check['Left'](invert(field))
@@ -157,8 +194,9 @@ class GameField(object):
 
         check['Down'] = lambda field: \
             check['Right'](transpose(field))
-
+        # 判断矩阵每一行有没有可以右移动的元素。这里只用进行判断，所以矩阵变换之后，不用再变换复原
         if direction in check:
+            # 传入矩阵，执行对应函数 
             return check[direction](self.field)
         else:
             return False
